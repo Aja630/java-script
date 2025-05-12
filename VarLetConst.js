fe0ac47a7e7135b9389a8d6e8b10db27bc5b78d0
Aja630 <ajayahirwar1999.srj@gmail.com>
2025-05-12 19:32:56 +0530

    up

--- VarLetConst.js
+++ VarLetConst.js
@@ -1,15 +1,54 @@
-function ajay(){
+ function ajay(){
 
     // Redeclaration
 
-    var a=10
-    var a=18
+    // var a=10
+    // var a=18
+
+    // let b=10
+    // let b=15
+
+    // const b=7
+    // const b=5
+
+    //====Re-assignment
 
 
+    //  var age=5
+    //  age=6
 
- console.log(a);
+    // let age=5
+    // age=6
 
+//   const age=5
+//   age=7;
+
+
+  //======Scope
+
+//   let a=7
+//   {
+//     let a=8
+//     console.log(a);
+
+//   }
+//  console.log(a);
+
+
+//===============Hoisting
+
+// console.log(a);
+// var a=5        //undefine
+// let a=5          //error   
+// }
+
+// ajay()
+
+
+//===============DOM(Document Object Model)(it is a tree life structure)
+
+
+    alert("okk aggya");
 
 }
-
 ajay()
---
+++ string.js
@@ -0,0 +1,30 @@
+// let text="            this is string"
+// console.log(text.trim());
+
+// let text="this is string"
+// console.log(text.indexOf("i"));  //indexof me 0 se start krte hai 
+//output 2
+
+
+
+//replace
+function replace(){
+    
+
+// let text= "This is String and string of JS"
+// console.log(text.replace("string","Cybrom"));
+// console.log(text.replaceAll("string","Cybrom"));
+// console.log(text.replace(/string/gi,"Cybrom"));
+
+
+
+
+
+//let Fruits=["Apple","Banana"] //Array
+
+let okk= "W-e are f-rom c-ybr-om"
+// console.log(okk.split(" "));
+console.log(okk.split("-"));
+
+}
+replace()
