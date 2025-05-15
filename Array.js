822d32fcce02c64fc9d4f73a81084ef1c815fe80
Aja630 <ajayahirwar1999.srj@gmail.com>
2025-05-15 19:27:11 +0530

    up

---
+++ Array.js
@@ -0,0 +1,23 @@
+
+// let Fruits= [ "Apple" ,"Mango", "Banana","Grapes" ]
+// let ans= Fruits.slice(1,3)
+// Fruits.splice(2,0,"papaya")   //where to add , how many to delete, what to add
+// let arr1=[1,4,6,7]
+
+// console.log(arr1.reverse());
+// let arr2=[2,6,8,9]
+// let arr3=[5,8,3,2]
+// console.log(arr1.concat(arr2,arr3));
+// let num=75638
+// console.log(Number(num.toString().split('').reverse().join('')));
+
+
+
+//  console.log(Fruits);
+
+
+
+//Destructuring
+let arr=["Apple","Mango","Banana","Grapes"]
+const [a,,b,c]=arr //Destructuring
+console.log(c);
--- string.js
+++ string.js
@@ -1,3 +1,8 @@
+// function str(){
+
+
+
+
 // let text="            this is string"
 // console.log(text.trim());
 
@@ -5,10 +10,11 @@
 // console.log(text.indexOf("i"));  //indexof me 0 se start krte hai 
 //output 2
 
-
+// }
+// str()
 
 //replace
-function replace(){
+// function replace(){
     
 
 // let text= "This is String and string of JS"
@@ -22,9 +28,50 @@ function replace(){
 
 //let Fruits=["Apple","Banana"] //Array
 
-let okk= "W-e are f-rom c-ybr-om"
+// let okk= "W-e are f-rom c-ybr-om"
 // console.log(okk.split(" "));
-console.log(okk.split("-"));
+// console.log(okk.split("-"));
 
-}
-replace()
+
+
+// }
+// replace()
+
+
+
+//Template letrals(` `)
+//It Support Single quates('') and Double("")
+
+//let str= ` i am 'from' Bhopal `
+//It Support multi line string
+
+
+//let okkk= `Hello p52 batch
+ //from cybrom`
+
+ //It Support HTML Tags
+//  function ajay() {
+//     let str= `<h1> Hello world </h1>`
+//  document.write(str)
+
+//  }
+//  ajay()
+
+
+//let Name=12
+//let Age=21
+
+//alert( "I am "+ Name+ "and i am " +Age )
+// alert(` i am ${Name} and i am ${Age} years Old `)
+//alert(`sum of ${Name} and ${Age} is ${Name+Age}`)
+
+
+
+
+// function ajay(){
+
+//     alert(`we are from "cybrom"which is in 'Bhopal'`)
+// }
+// ajay()
+
+
