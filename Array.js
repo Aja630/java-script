d07c4342f8465096018e2623372519fb1241faae
Aja630 <ajayahirwar1999.srj@gmail.com>
2025-05-16 19:31:33 +0530

    up

--- Array.js
+++ Array.js
@@ -15,9 +15,67 @@
 
 //  console.log(Fruits);
 
-
-
 //Destructuring
-let arr=["Apple","Mango","Banana","Grapes"]
-const [a,,b,c]=arr //Destructuring
-console.log(c);
+// let arr=["Apple","Mango","Banana","Grapes"]
+// const [a,,b,c]=arr //Destructuring
+// console.log(c);
+
+
+//sum of all value
+// let ar=[2,4,5,6,7]
+// let sum=0;
+// for(let i=0;i<ar.length;i++){
+//     sum=sum+ar[i];
+// }
+// console.log(sum)
+
+
+
+//maximum value find krne ke liye
+// let arr=[3,7,2,9,5]
+// let max=arr[0]
+// for(let i=1;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max=arr[i]
+//     }
+// }
+// console.log(max)
+
+
+
+//even or odd no. batane ke liye
+// let arr=[1, 2, 3, 4, 5, 6, 7, 8]
+// let even = 0
+// let odd = 0
+// for(let i of arr){
+//     if(i%2 == 0){
+//         even +=1
+//     }
+// }
+// console.log(even)
+// console.log(odd)
+
+
+
+//reverse no. find karne ke liye
+// let arr=[10, 20, 30, 40, 50]
+// let reverse = []
+// for( let i=arr.length-1; i>=0;i--){
+//     reverse.push(arr[i])
+
+// }
+// console.log(reverse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
