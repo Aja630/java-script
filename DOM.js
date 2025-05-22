1c52f8bc025f268d5587b1d21cf2d199304399d7
Aja630 <ajayahirwar1999.srj@gmail.com>
2025-05-22 19:26:22 +0530

    up

---
+++ DOM.js
@@ -0,0 +1,22 @@
+
+
+let callme=()=>{
+//method
+//let mytag=  document.getElementById("ajay")
+  //  let mana=   document.getElementsByClassName("ahirwar")
+  let aja=   document.querySelector("#ajay")
+
+   let aja1=     document.querySelector("#ahirwar")
+
+      //let mytag=  document.querySelector("#text")
+
+     aja.innerHTML="<p> ajay </p>"
+        aja1.innerHTML="<p> ahirwar </p>"
+        
+     //mana.innerHTML="ahirwar"
+     // mytag.textContent=""
+
+    
+}
+
+
