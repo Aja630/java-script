78d15478ca9db77eb5c62681fd90472f60b2da79
Aja630 <ajayahirwar1999.srj@gmail.com>
2025-05-28 20:31:34 +0530

    up

--- DOM.js
+++ DOM.js
@@ -10,7 +10,7 @@ let callme=()=>{
 
       //let mytag=  document.querySelector("#text")
 
-     aja.innerHTML="<p> sandeep </p>"
+     aja.innerHTML="<p> sun </p>"
         aja1.innerHTML="<p> jatav </p>"
 
         aja.Style.
